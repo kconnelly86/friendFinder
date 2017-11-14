27800ca01dfbbed9758c1090f1002326ddc8491e
kconnelly86 <kconnelly86@yahoo.com>
2017-11-13 21:01:54 -0600

    not complete, still working on form

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,95 +1,209 @@
-// Chosen CSS
-    var config = {
-      '.chosen-select'           : {},
-      '.chosen-select-deselect'  : {allow_single_deselect:true},
-      '.chosen-select-no-single' : {disable_search_threshold:10},
-      '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
-      '.chosen-select-width'     : {width:"95%"}
-    }
+var friends = [
+    {
+        name: "Thor",
+        photo:
+        scores:[
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "2"
+        ]
+    },
+    {
+        name: "Captain America",
+        photo:
+        scores:[
+        "3",
+        "5",
+        "3",
+        "4",
+        "3",
+        "4",
+        "5",
+        "1",
+        "1",
+        "5"
+        ]
+    },
+    {
+        name: "Iron Man",
+        photo:
+        scores:[
+        "5",
+        "4",
+        "5",
+        "2",
+        "4",
+        "3",
+        "4",
+        "2",
+        "3",
+        "4"
+        ]
+    },
+    {
+        name: "Spider Man",
+        photo:
+        scores:[
+        "3",
+        "4",
+        "3",
+        "2",
+        "4",
+        "3",
+        "4",
+        "4",
+        "2",
+        "3"
+        ]
+    },
+    {
+        name: "Hulk",
+        photo:
+        scores:[
+        "5",
+        "4",
+        "5",
+        "4",
+        "4",
+        "5",
+        "4",
+        "5",
+        "4",
+        "4"
+        ]
+    },
+
+
+];//ends var array
+
+module.exports = friends;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Chosen CSS
+//     var config = {
+//       '.chosen-select'           : {},
+//       '.chosen-select-deselect'  : {allow_single_deselect:true},
+//       '.chosen-select-no-single' : {disable_search_threshold:10},
+//       '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
+//       '.chosen-select-width'     : {width:"95%"}
+//     }
 
 
     
-    for (var selector in config) {
-      $(selector).chosen(config[selector]);
-    }
+//     for (var selector in config) {
+//       $(selector).chosen(config[selector]);
+//     }
 
-    // Capture the form inputs 
-    $("#submit").on("click", function(){
+//     // Capture the form inputs 
+//     $("#submit").on("click", function(){
 
-      // Form validation
-      function validateForm() {
-      var isValid = true;
-      $('.form-control').each(function() {
-        if ( $(this).val() === '' )
-            isValid = false;
-      });
+//       // Form validation
+//       function validateForm() {
+//       var isValid = true;
+//       $('.form-control').each(function() {
+//         if ( $(this).val() === '' )
+//             isValid = false;
+//       });
 
-      $('.chosen-select').each(function() {
+//       $('.chosen-select').each(function() {
 
-        if( $(this).val() === "")
-          isValid = false
-      })
-      return isValid;
-    }
+//         if( $(this).val() === "")
+//           isValid = false
+//       })
+//       return isValid;
+//     }
 
-    // If all required fields are filled
-    if (validateForm() == true)
-    {
-      // Create an object for the user's data
-        var userData = {
-          name: $("#name").val(),
-          photo: $("#photo").val(),
-          scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val(), ],
+//     // If all required fields are filled
+//     if (validateForm() == true)
+//     {
+//       // Create an object for the user's data
+//         var userData = {
+//           name: $("#name").val(),
+//           photo: $("#photo").val(),
+//           scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val(), ],
 
-        }
+//         }
 
-         var scoreArray = userData.scores;
-         console.log(scoreArray);
-         var totalScore = 0;
-         var userGrade ="";
-         for (var i = 0; i < scoreArray.length; i++) {
-           totalScore += parseInt(scoreArray[i]);
-         }
-         if(totalScore >= 10 && totalScore <=20) {
-            userGrade = "a"
-            userData["userGrade"] = userGrade;
-         } else if (totalScore >= 21 && totalScore <= 30) {
-            userGrade = "b";
-            userData["userGrade"] = userGrade;
-         } else if (totalScore >= 31 && totalScore <= 40) {
-            userGrade = "c";
-            userData["userGrade"] = userGrade;
-         }else  {
-            userGrade = "d";
-            userData["userGrade"] = userGrade;
-         };
+//          var scoreArray = userData.scores;
+//          console.log(scoreArray);
+//          var totalScore = 0;
+//          var userGrade ="";
+//          for (var i = 0; i < scoreArray.length; i++) {
+//            totalScore += parseInt(scoreArray[i]);
+//          }
+//          if(totalScore >= 10 && totalScore <=20) {
+//             userGrade = "a"
+//             userData["userGrade"] = userGrade;
+//          } else if (totalScore >= 21 && totalScore <= 30) {
+//             userGrade = "b";
+//             userData["userGrade"] = userGrade;
+//          } else if (totalScore >= 31 && totalScore <= 40) {
+//             userGrade = "c";
+//             userData["userGrade"] = userGrade;
+//          }else  {
+//             userGrade = "d";
+//             userData["userGrade"] = userGrade;
+//          };
          
-         console.log(userData);
+//          console.log(userData);
          
 
 
 
-        // AJAX post the data to the friends API.
+//         // AJAX post the data to the friends API.
 
-          // Grab the URL of the website
-        var currentURL = window.location.origin;
+//           // Grab the URL of the website
+//         var currentURL = window.location.origin;
 
-        // AJAX post the data to the friends API. 
-        $.post(currentURL + "/api/friends", userData, function(data){
-         for (var i = 0; i < data.length; i++) {
-           $("#modal-body").append("<h2>"+data[i].name+"</h2>");
-           $("#modal-body").append("<img src="+data[i].photo+"></img>");
-         };
-         // Show the modal with the best match
-         $("#resultsModal").modal('toggle');
-       });
+//         // AJAX post the data to the friends API. 
+//         $.post(currentURL + "/api/friends", userData, function(data){
+//          for (var i = 0; i < data.length; i++) {
+//            $("#modal-body").append("<h2>"+data[i].name+"</h2>");
+//            $("#modal-body").append("<img src="+data[i].photo+"></img>");
+//          };
+//          // Show the modal with the best match
+//          $("#resultsModal").modal('toggle');
+//        });
 
         
-    }
-    else
-    {
-      alert("Please fill out all fields before submitting!");
-    }
+//     }
+//     else
+//     {
+//       alert("Please fill out all fields before submitting!");
+//     }
       
-      return false;
-    });
+//       return false;
+//     });
--- app/server.js
+++ app/server.js
@@ -13,76 +13,6 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
-var characters = [
-  
-
-{
-"name": "neko",
-"photo": "wwer",
-"scores": [
-2,
-2,
-2,
-3,
-2,
-2,
-2,
-2,
-2,
-2
-]
-},
-{
-"name": "neko",
-"photo": "aer",
-"scores": [
-2,
-2,
-2,
-2,
-3,
-2,
-2,
-2,
-3,
-2
-]
-},
-{
-"name": "neko",
-"photo": "aer",
-"scores": [
-2,
-2,
-2,
-2,
-3,
-2,
-2,
-2,
-3,
-2
-]
-},
-{
-"name": "neko",
-"photo": "aer",
-"scores": [
-2,
-2,
-2,
-2,
-3,
-2,
-2,
-2,
-3,
-2
-]
-}
-];
 
 // Routes
 // =============================================================
@@ -96,41 +26,41 @@ app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/survey.html"));
 });
 
-// Get all characters
+// Get all friends
 app.get("/friends", function(req, res) {
-  res.json(characters);
+  res.json(friends);
 });
 
-// Search for Specific Character (or all characters) - provides JSON
+// Search for Specific friends (or all friends) - provides JSON
 app.get("/api/friends", function(req, res) {
-  var chosen = req.params.characters;
+  var chosen = req.params.friends;
 
   if (chosen) {
     console.log(chosen);
 
-    for (var i = 0; i < characters.length; i++) {
-      if (chosen === characters[i].routeName) {
-        return res.json(characters[i]);
+    for (var i = 0; i < friends.length; i++) {
+      if (chosen === friends[i].routeName) {
+        return res.json(friends[i]);
       }
     }
     return res.json(false);
   }
-  return res.json(characters);
+  return res.json(friends);
 });
 
-// Create New Characters - takes in JSON input
+// Create New friends - takes in JSON input
 app.post("/api/new", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body-parser middleware
-  var newcharacter = req.body;
-  characters.push(newcharacter);
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  var newfriends = req.body;
+  friends.push(newfriends);
+  newfriends.routeName = newfriends.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newcharacter);
+  console.log(newfriends);
 
  
 
-  res.json(newcharacter);
+  res.json(newfriends);
 });
 
 // Starts the server to begin listening
